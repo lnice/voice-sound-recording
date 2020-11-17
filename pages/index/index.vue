27a4fec88e23c0929fdb3461d7a8d16b58529a5d
anil <993421281@qq.com>
2020-11-18 01:28:39 +0800

    初始化项目

---
+++ pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="content">
+    <button type="primary" @click="show = true">语音录制</button>
+    <view class="popup-bottom" v-if="show">
+      <view class="popup-bg" @click="show = false"></view>
+      <view class="popup-content">
+        <sound-recording
+          :maximum="15"
+          :duration="100"
+          @cancel="show = false"
+          @confirm="onUpload">
+        </sound-recording>
+      </view>
+    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+        show: false
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+      onUpload (tempFilePath) {
+        console.log(tempFilePath)
+      }
+		}
+	}
+</script>
+
+<style lang="scss">
+  .content {
+    padding: 30rpx;
+  }
+  .popup-bottom {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+    .popup-bg {
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0,0,0,0.5);
+    }
+    .popup-content {
+      height: 40%;
+      margin-top: auto;
+      background-color: #fff;
+      position: relative;
+      z-index: 11;
+    }
+  }
+</style>
